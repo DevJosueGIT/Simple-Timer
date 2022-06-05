28e1f1185e7f927de860e2ec2a34607dee6f14d8
Josue <josueytcm@gmail.com>
2022-06-05 17:21:10 -0500

    Only one commit

---
+++ components/main.js
@@ -0,0 +1,28 @@
+const time = document.getElementById("time");
+const date = document.getElementById("date");
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const interval = setInterval( () => {
+    const local = new Date()
+
+    let day = local.getDate(),
+        month = local.getMonth(),
+        year = local.getFullYear();
+
+    time.innerHTML = local.toLocaleTimeString();
+    date.innerHTML = `${day} ${months[month]} ${year}`   
+},1000)
